3296813c68af37c6420a5b56a8f86557a46f04b5
albert Byverdu :) <byverdu@gmail.com>
2017-03-01 00:17:40 +0000

    shy start with header components

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './title';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <Title name="Residència Ergos" />
+      </header>
+    )
+  }
+}
+
+export default Header;
